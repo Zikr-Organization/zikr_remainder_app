7ce5c085782e37cd070b17c5b75a92bbe5af9093
MD Fahid Sarker <1155149075@link.cuhk.edu.hk>
2022-01-09 16:38:01 +0800

    initial commit

---
+++ android/app/src/main/kotlin/org/zikr/zikr_remainder_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.zikr.zikr_remainder_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:zikr_remainder_app/screens/homescreen/home_screen.dart';
+
+main(){
+  runApp(const HomeScreen());
+}
