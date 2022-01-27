7cb530e6826b4f51f536b5e7b36eba03b06f3950
Yousra Ahmed <yosra.ahmed.ibrahim@gmail.com>
2022-01-27 23:28:15 +0200

    The theme of project, appbar, tab bar are modified. The Screen of Hisn el muslim athkar ui is modified but without all its functionallity. Some Images are added and icons.

--- lib/screens/homescreen/home_screen.dart
+++ lib/screens/homescreen/home_screen.dart
@@ -6,8 +6,61 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ZikrCategoryScreen()
+    return MaterialApp(
+        title: 'Zikr Reminder',
+        theme: ThemeData(
+          // Define the default brightness and colors.
+         // brightness: Brightness.dark,
+          primaryColorDark: Color(0xff040722),
+          primaryColor: Color(0xff212e54),
+          primaryColorLight: Color(0xff314879),
+          appBarTheme: const AppBarTheme(
+            color: Color(0xff040722),
+            foregroundColor: Color(0xff8b7b52)
+          ),
+          tabBarTheme: const TabBarTheme(
+                //backgroundColor: Color(0xff040722),
+                labelColor: Color(0xff8b7b52),
+                labelStyle: TextStyle(color: Color(0xff8b7b52)), // color for text
+                indicator: UnderlineTabIndicator( // color for indicator (underline)
+                    borderSide: BorderSide(color: Color(0xff8b7b52)),
+                ),
+            ),
+
+
+          // Define the default font family.
+        //  fontFamily: 'Georgia',
+
+          // Define the default `TextTheme`. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+     /*     textTheme: const TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold, color: Color(0xff544721)),
+            headline6: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold, color: Color(0xff8b7b52)),
+            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Color(0xffd5c296)),
+          ),*/
+        ),
+      home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.book)),
+              Tab(icon: Icon(Icons.edit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+          title: const Text('Zikr Reminder'),
+        ),
+        body: const TabBarView(
+          children: [
+            ZikrCategoryScreen(),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
+      ),
+    ),
     );
   }
 }
--- lib/screens/zikr_screen/category/ZikrCategoryScreen.dart
+++ lib/screens/zikr_screen/category/ZikrCategoryScreen.dart
@@ -3,37 +3,140 @@ import 'package:zikr_remainder_app/models/zikr_category.dart';
 import 'package:zikr_remainder_app/repository/api.dart';
 import 'package:zikr_remainder_app/screens/widgets/loading.dart';
 
-class ZikrCategoryScreen extends StatelessWidget {
+class ZikrCategoryScreen extends StatefulWidget {
   const ZikrCategoryScreen({Key? key}) : super(key: key);
 
   @override
+  _ZikrCategoryScreenState createState() => _ZikrCategoryScreenState();
+}
+
+class _ZikrCategoryScreenState extends State<ZikrCategoryScreen> {
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Zikr Categories Test'),),
-      body: FutureBuilder<List<ZikrCategory>>(
-        future: allZikrCategories,
-        builder: (_, snap) {
-          final data = snap.data;
-          if (data == null) {
-            return const Loading();
-          }
-          return ListView.builder(
-            itemBuilder: (_, i) => InkWell(
-              onTap: () {
-                //navigate to details screen
-              },
-              child: Container(
-                color: i % 2 == 0 ? Colors.grey.withOpacity(0.5) : null,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(data[i].title),
+    return MaterialApp(
+      title: 'Zikr Categories Test',
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/vector-1-background.jpg"),
+                fit: BoxFit.cover)),
+        //child: Text("Allah"),
+   //     child: Scaffold(
+   //       backgroundColor: Colors.transparent,
+   //       appBar: AppBar(
+  //          elevation: 0,
+  //          backgroundColor: Colors.transparent,
+  //        ),
+          //   appBar: AppBar(title: Text('Zikr Categories Test'),),
+          child: FutureBuilder<List<ZikrCategory>>(
+            future: allZikrCategories,
+            builder: (_, snap) {
+              final data = snap.data;
+              if (data == null) {
+                return const Loading();
+              }
+              return ListView.builder(
+                itemBuilder: (_, i) => InkWell(
+                  onTap: () {
+                    //navigate to details screen
+                  },
+                  child: Container(
+                    color: Colors.transparent,
+                    child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ZikrConditionRow(
+                          isPreferd: false,
+                          title: data[i].title,
+                          hasAlarm: (data[i].id == 27 && data[i].id == 28 && data[i].id == 1),
+                          alarmSet: '08:00',
+                        ) //Text(data[i].title),
+                        ),
+                  ), 
                 ),
-              ), //todo rewrite UI here
-            ),
-            itemCount: data.length,
-          );
-        },
+                itemCount: data.length,
+              );
+            },
+          ),
+       // ),
       ),
+    );
+  }
+}
+
+class ZikrConditionRow extends StatelessWidget {
+  const ZikrConditionRow(
+      {required this.isPreferd, required this.title, required this.hasAlarm, this.alarmSet = null});
+  final bool isPreferd;
+  final String title;
+  final bool hasAlarm;
+  final String? alarmSet;
+
+  List<Widget> getRowChildren() {
+    List<Widget> children = [
+    Expanded(
+    flex: 5,
+    child: IconButton(
+    icon: Image.asset(isPreferd
+    ? 'images/love-marked.png'
+        : 'images/love-not-marked.png'),
+    iconSize: 50,
+    onPressed: () {},
+    ),
+    ),
+    Expanded(
+    flex: 16,
+    child: Text(
+    title,
+    style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.w500, color: Colors.white, decoration: TextDecoration.none),
+
+    ),
+    ),
+
+    ];
+    if (this.hasAlarm && this.alarmSet == null){
+      children.add(Expanded(
+        flex: 3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Image.asset('images/alarm-clock.png'),
+              iconSize: 30,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ))
+      ;
+    }
+    else {
+      children.add(Expanded(
+        flex: 3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(alarmSet!, style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Color(0xff8b7b52), decoration: TextDecoration.none),),
+            IconButton(
+              icon: Image.asset('images/alarm-clock.png'),
+              iconSize: 30,
+              onPressed: () {},
+            ),
+        ],
+         ),
+      ),
+    );
+    }
+    return children;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          height: 100.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: getRowChildren(),
+          ),
     );
   }
 }
